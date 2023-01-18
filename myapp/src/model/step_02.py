04fa0333202018fda95b4ee0cbbb3085a3a94b38
lpaes <lpaes@connexi.com>
2023-01-18 10:17:13 +0000

    implemented the XGboost class

--- myapp/src/model/step_02.py
+++ myapp/src/model/step_02.py
@@ -1,19 +1,33 @@
+#system
 import glob
 import traceback
+
+# data prep stack
 import pandas as pd
 import numpy as np
-from fast_tabnet.core import *
-from fastai.tabular.all import *
 
+#TabNet
+from fast_tabnet.core import *
+#FastAi
+from fastai.tabular.all import *
 
 #LazyPredict
 import lazypredict
 from lazypredict.Supervised import LazyRegressor
 from lazypredict.Supervised import LazyClassifier
 
+#Fit an xgboost model
+from xgboost import XGBRegressor
+from xgboost import XGBClassifier
+from xgboost import plot_importance
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import roc_auc_score
+
 #internal imports
 from ml_settings import MlSettings
 from step_01 import MLDataInput
+
+
 
 class TrainModel:
 
@@ -65,7 +79,7 @@ class TrainModel:
                 models['target_std'] = target_std
 
                 #rename index of 
-                models.to_csv(f'{self.PARAM_DIR}/regression_results_{target}.csv', mode='a', header=True, index=True)
+                models.to_csv(f'{self.PARAM_DIR}/regression_results_{target}.csv', mode='a', header=True, index=True)#TODO create a new folder
             except:
                 print('Issue during lazypredict analysis')
     
@@ -144,7 +158,41 @@ class TrainModel:
             traceback.print_exc()
             pass
 
+    def fit_xgboost_model(self, target, X_train, X_test, y_train, y_test):
+        
+        if MlSettings.REFRESSOR == True:
+            xgb = XGBRegressor()
+        else:
+            xgb = XGBClassifier
+        
+        try:
+            xgb.fit(X_train, y_train)
+            y_pred = xgb.predict(X_test)
+            print('XGBoost Predictions vs Actual==========')
+            print(pd.DataFrame({'actual': y_test.iloc[:,0].values, 'predicted': y_pred}).head())
+            print('XGBoost RMSE: ', np.sqrt(mean_squared_error(y_test, y_pred)))
 
+            #save feature importance plot to file
+            plot_importance(xgb)
+            plt.title(f'XGBoost Feature Importance for {MlSettings.PROJECT_NAME} | Target : {target}', wrap=True)
+            plt.tight_layout()
+            plt.show()
+            plt.savefig(f'{self.PARAM_DIR}/xgb_feature_importance_{target}.png') #TODO inswet a new folder
+
+            fi_df = pd.DataFrame([xgb.get_booster().get_score()]).T
+            fi_df.columns = ['importance']
+
+            #create a column based off the index called feature
+            fi_df['feature'] = fi_df.index
+            # create a dataframe of feature importance
+            fi_df = fi_df[['feature','importance']]
+            fi_df.to_csv(f'{self.PARAM_DIR}/xgb_feature_importance_{target}.csv', index=False)#TODO create a folder
+          
+        except: 
+            traceback.print_exc()
+            print('XGBoost failed')
+              
+        
 
 
     def main(self):
@@ -159,12 +207,13 @@ class TrainModel:
             #LazyRegressor models
             self.model_selection(target, X_train, X_test, y_train, y_test)
             
-            #TabNet models
-            tabnet_vars = self.tabnel_model_selection(target)
-            print()
+            # #TabNet models
+            # tabnet_vars = self.tabnel_model_selection(target)          
+            # #Tabnet - Auto learning set
+            # self.auto_learning_setting(x=tabnet_vars['x'],i=tabnet_vars['i'],learn=tabnet_vars['learn'])
 
-            #Auto learning set
-            self.auto_learning_setting(x=tabnet_vars['x'],i=tabnet_vars['i'],learn=tabnet_vars['learn'])
+            #Fit XGBoost
+            self.fit_xgboost_model(target, X_train, X_test, y_train, y_test)
         return
         
 
